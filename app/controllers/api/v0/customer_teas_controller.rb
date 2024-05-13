5bf2f6ac6c198f49c26fc2fcc802c0d14c8ad110
Matt Darlington <matthewsdarlington@gmail.com>
2024-05-13 09:43:18 -0600

    Add update action to handle cancelling a customer's subscription

--- app/controllers/api/v0/customer_teas_controller.rb
+++ app/controllers/api/v0/customer_teas_controller.rb
@@ -8,6 +8,15 @@ class Api::V0::CustomerTeasController < ApplicationController
 		end
 	end
 
+	def update
+		subscription = Subscription.find_by(customer_id: params[:customer_tea][:customer_id], tea_id: params[:customer_tea][:tea_id])
+		if subscription && subscription.update(status: 1)
+			render json: SubscriptionSerializer.new(subscription), status: :ok
+		else
+			render json: { error: "Subscription not found" }, status: :not_found
+		end
+	end
+
 	private
 
 	def customer_tea_params
