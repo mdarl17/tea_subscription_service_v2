f920ad24fd761d2b74bf06d5c6baf94e7edab0b2
Matt Darlington <matthewsdarlington@gmail.com>
2024-05-13 09:45:41 -0600

    Remove integer format validation from status and frequency

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,7 +2,7 @@ class Subscription < ApplicationRecord
 	belongs_to :customer
 	belongs_to :tea
 
-	validates :status, :frequency, presence: true, numericality: { only_integer: true }
+	validates :status, :frequency, presence: true
 
 	enum status: {"active": 0, "cancelled": 1}
 	enum frequency: {"weekly": 0, "monthly": 1, "quarterly": 2}
