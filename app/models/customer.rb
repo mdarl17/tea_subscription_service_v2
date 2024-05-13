d01ecd0c8d7b88a155d7cf047ec44e1712be6b2a
Matt Darlington <matthewsdarlington@gmail.com>
2024-05-12 23:50:51 -0600

    Reapply "Add RSpec unit test for customer active record resource; includes validation and relationship testing"
    This reverts commit e3ac78268916a30eb688a18101786a26cc6cd1a7.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,7 +1,4 @@
 class Customer < ApplicationRecord
 	has_many :subscriptions
 	has_many :teas, through: :subscriptions
-
-	validates :first_name, :last_name, :address, :api_key, presence: true
-	validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
